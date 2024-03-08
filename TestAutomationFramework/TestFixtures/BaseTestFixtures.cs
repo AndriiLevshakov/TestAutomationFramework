eb8dc54345ce6f55763a99bdc061174039c9346a
Andrii Levshakov <andrii_levshakov@epam.com>
2024-03-08 10:43:24 +0200

    AppSettings added

--- TestAutomationFramework/TestFixtures/BaseTestFixtures.cs
+++ TestAutomationFramework/TestFixtures/BaseTestFixtures.cs
@@ -1,11 +1,22 @@
 ﻿using Core;
 using System.Configuration;
+using Microsoft.Extensions.Configuration;
 
 namespace TestLayer
 {
     public class BaseTestFixtures
     {
-        public string BaseUrl { get; } = ConfigurationManager.AppSettings["BaseUrl"];
+        protected IConfigurationRoot Configuration { get; }
+
+        public BaseTestFixtures() 
+        {
+            var builder = new ConfigurationBuilder()
+                .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true);
+
+            Configuration = builder.Build();
+        }
+
+        protected string BaseUrl => Configuration.GetSection("AppSettings")["BaseUrl"];
 
         [SetUp]
         public void SetUp()
